8491b44df52d67daded134776e79521758204524
Ajay Singh Tanwar <Ajay Singh Tanwar>
2017-11-03 21:30:05 -0700

    added serialization

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -3,18 +3,21 @@
 #include <stdlib.h>
 
 #include "../../engine/engine.h"
+#include "../../engine/serializer/serializer.h"
 
 bool run = true;
 //game start point
 int main(int argc, char** argv) {
-
 
 	std::cout << "Game STart Point " << std::endl;
 	if (run) {
 
 		enginecore::Engine::GetInstance();
 		run = false;
+		enginecore::serialize::Serializer::GetInstance()->SerializeGameData("playground/resources/gamedata/player.json");
+		enginecore::Engine::GetInstance()->Run();
 	}
+
 
 	return 0;
 }
